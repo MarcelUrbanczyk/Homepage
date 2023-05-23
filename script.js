1e61b3e0d0c339d5815df34b42c4b428448acaaa
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-05-23 18:30:19 +0200

    Improved dark theme links and added hover effects to navigation buttons

--- script.js
+++ script.js
@@ -2,14 +2,17 @@
   const themeSwitchElement = document.getElementById("themeSwitch");
 
   const themeSwitching = () =>{
+    const movieLinks = document.querySelectorAll(".sectionMovies__a")
     const body = document.getElementById("body");
     const separator = document.querySelectorAll(".main__separator");
     themeSwitchElement.classList.toggle("light");
     themeSwitchElement.classList.toggle("themeSwitch--opacityDark");
     body.classList.toggle("body--dark");
     separator.forEach((separator) => {
-      separator.classList.toggle("main__separator--lightTheme");
       separator.classList.toggle("main__separator--darkTheme");
+    });
+    movieLinks.forEach((movieLinks) => {
+      movieLinks.classList.toggle("sectionMovies__a--darkTheme");
     });
   }
   
