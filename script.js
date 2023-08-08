821e6c9aa833b130d7e9fd315277d228e666ddfd
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-08-08 21:40:24 +0200

    minor changes

--- script.js
+++ script.js
@@ -1,21 +1,16 @@
-{
-  const themeSwitchElement = document.getElementById("themeSwitch");
+let themeSwitchElement = document.getElementById("themeSwitch");
+let body = document.getElementById("body");
+let separator = document.querySelectorAll(".main__separator");
 
-  const themeSwitching = () =>{
-    const movieLinks = document.querySelectorAll(".sectionMovies__a")
-    const body = document.getElementById("body");
-    const separator = document.querySelectorAll(".main__separator");
-    themeSwitchElement.classList.toggle("light");
-    themeSwitchElement.classList.toggle("themeSwitch--opacityDark");
-    body.classList.toggle("body--dark");
-    separator.forEach((separator) => {
-      separator.classList.toggle("main__separator--darkTheme");
-    });
-    movieLinks.forEach((movieLinks) => {
-      movieLinks.classList.toggle("sectionMovies__a--darkTheme");
-    });
-  }
-  
- themeSwitchElement.addEventListener('click',themeSwitching)
+themeSwitchElement.addEventListener("click", () => {
+  themeSwitchElement.classList.toggle("light");
 
-}
+  body.classList.toggle("body--dark");
+
+  themeSwitchElement.classList.toggle("themeSwitch--opacityDark");
+
+  separator.forEach((separator) => {
+    separator.classList.toggle("main__separator--lightTheme");
+    separator.classList.toggle("main__separator--darkTheme");
+  });
+});
